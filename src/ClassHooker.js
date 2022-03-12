2ade6c9efee6ddc234fcc5f82e24d29dd1631b7d
aafrinabid <mohdaafrin@gmail.com>
2022-03-12 17:17:18 +0530

    hoooooks

---
+++ src/ClassHooker.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+export class ClassHooker extends Component {
+state={
+    count:0
+}
+componentDidMount(){
+    this.interval=setInterval(this.tick,1000)
+}
+componentWillUnmount(){
+    clearInterval(this.interval)
+}
+tick=()=>{
+    this.setState({
+        count:this.state.count+1
+    })
+}
+    render() {
+    return (
+      <div>
+          <h1>{this.state.count}</h1>
+      </div>
+    )
+  }
+}
+
+export default ClassHooker
